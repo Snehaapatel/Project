cfcda84d8e99d1c2dd3b06212a05f5366d9a7888
Snehaapatel <patelsneha689@gmail.com>
2024-05-24 21:23:58 +0530

    User Management Api

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "TXNQzh7dckQ/Kb7WXhw2IaYI/cHd7y7VRezaLHeWOQs=",
+    EMAIL: "jennifer.lakin43@ethereal.email", // testing email & password
+    PASSWORD : "pWAvAba3desGNEhET2",
+    ATLAS_URI: "mongodb+srv://admin:admin123@cluster0.r1itbym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+}
